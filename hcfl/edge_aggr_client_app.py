f4adaf16803587d3cacac568524418ef85572199
Dorin Flocos <dflocos@gmail.com>
2025-07-15 02:00:05 -0400

    fix saving/loading weights

--- hcfl/edge_aggr_client_app.py
+++ hcfl/edge_aggr_client_app.py
@@ -1,26 +1,25 @@
 import os
-import numpy as np
+import pickle
 
 from flwr.client import NumPyClient, ClientApp
-from flwr.common import Context, ndarrays_to_parameters, parameters_to_ndarrays
+from flwr.common import Context
 
 
 class EdgeAggrClient(NumPyClient):
 
     def fit(self, parameters, config):
-        filepath = "weights.npz"
-        print(f"saving global model weights to file: {filepath}")
-        np.savez(filepath, *parameters)
+        global_weights_filepath = "weights.pkl"
+        with open(global_weights_filepath, "wb") as file:
+            pickle.dump(parameters, file)
+        print(f"Global model weights saved: {global_weights_filepath}")
 
-        print(f"global model weights saved to {filepath}")
-
-        print("sending signal")
+        print("Sending signal...", end='')
         glb_aggr_pipe = "glb_aggr_sig"
         with open(glb_aggr_pipe, "w") as pipe:
             pipe.write("GLB_AGGR_W")  # -> Edge Aggregator server
-        print("done")
+        print("Done")
 
-        print("waiting for local training and aggregation to finish...")
+        print("Waiting for local training and aggregation to finish...")
 
         loc_aggr_pipe = "loc_aggr_sig"
         if not os.path.exists(loc_aggr_pipe):
@@ -28,21 +27,23 @@ class EdgeAggrClient(NumPyClient):
 
         with open(loc_aggr_pipe, "r") as pipe:
             signal = pipe.readline().strip()  # wait for LOC_AGGR_W from Edge Aggregator server
-        print(f"signal received: {signal}\n")
+        print(f"Signal received: {signal}\n")
         os.remove(loc_aggr_pipe)
 
-        print("loading aggregated weights from file")
-        aggr_weights_ndarrays = np.load("weights.npz")
-        print("done")
+        updated_weights_filepath = "weights.pkl"
+        print(f"Loading weights from file: {updated_weights_filepath}...", end='')
+        with open(updated_weights_filepath, "rb") as file:
+            updated_parameters_ndarrays = pickle.load(file)
+        print("Done")
 
-        print("removing weights file")
-        os.remove("weights.npz")
-        print("done")
+        print("Removing weights file... ", end='')
+        os.remove(updated_weights_filepath)
+        print("Done")
 
-        return aggr_weights_ndarrays, 1, {}
+        return updated_parameters_ndarrays, 1, {}
 
     def evaluate(self, parameters, config):
-        return 1.0, 1, {"accuracy": 0.0}
+        return 1.0, 1, {"accuracy": 0.0}  # TODO: replace placeholder return data with actual evaulation logic
 
 
 def client_fn(context: Context):
